869d3551707ab1aad4a392e4d53935640853ba59
Marvin Mick <104308189+marvin-mick@users.noreply.github.com>
2022-04-24 17:12:36 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<form method="post" action="form.php" role="form">
+          <input type="text" name="name" placeholder="Nom" value="<?php echo $name; ?>" required>
+          <input type="email" name="email" placeholder="Email" value="<?php echo $email; ?>" required>
+          <textarea type="text" name="text" placeholder="Message" required><?php echo $text; ?></textarea>
+          <input type="submit" name="Envoyer" value="Envoyer ma demande">
+        </form>
